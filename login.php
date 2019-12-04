0c993b346405ff2d1701949a680eca34241a5adf
Weston Gallant <wesgallant97@gmail.com>
2019-12-04 02:02:24 -0500

    Added login form

---
+++ login.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title> Login Form </title>
+<link rel="stylesheet" a href="css/style.css">
+</head>
+<body>
+	<div class="container">
+	<form>
+	<div class="form-input">
+		<input type="text" name="text" placeholder="Enter the User Name"/>
+			</div>
+	<div class="form-input">
+		<input type="password" name="password" placeholder="Password"/> 
+			</div>
+	<input type="submit" type="submit" value="LOGIN" class="btn-login"/>
+			</form>
+	</div> 
+</body>
+</html> 
+	
